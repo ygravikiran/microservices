5389d56e21a1e84b6904f4be6cb1f3bb6a147b39
ygravikiran <yg.ravikiran@gmail.com>
2020-07-09 14:32:13 +0530

    Update CustomerApplication.java

--- customer/src/main/java/com/boa/customer/CustomerApplication.java
+++ customer/src/main/java/com/boa/customer/CustomerApplication.java
@@ -15,8 +15,19 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaClient
 public class CustomerApplication {
 
+	// To deploy
+	//1) To start this program from eclipse
+	//2) build the jar , right click on the project
+	// Run As -> Maven build , pass argument (clean install -DskipTests -> Apply -> Run = This provides the customer-0.0.1-SNAPSHOT.jar under "E:\ygravikiran githubrepo\rpsboamsjuly2020\customer\customer\target"
+	// To run this jar, go to cmd to this folder E:\ygravikiran githubrepo\rpsboamsjuly2020\customer\customer\target
+	// And run the below command 
+	//java -jar customer-0.0.1-SNAPSHOT.jar
+	//-Dserver.port=7072 -Dspring.profiles.active=qa
+	//To test the Application:
+	//http://localhost:7070/swagger-ui.html
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
-	
+
 }
